bc12da5a469e48914e359b6d5f335bad51a8dacd
m-d-bowerman <mbowerman@mozilla.com>
2024-02-08 14:39:38 -0800

    Remove unused config class

--- jobs/kpi-forecasting/kpi_forecasting/configs/model_inputs/__init__.py
+++ jobs/kpi-forecasting/kpi_forecasting/configs/model_inputs/__init__.py
@@ -2,7 +2,6 @@ import attr
 from typing import List, Dict, Optional, Union
 from pathlib import Path
 
-import toml
 import pandas as pd
 
 from kpi_forecasting.inputs import YAML
@@ -58,73 +57,3 @@ class ModelConfig:
         "parallel": "processes",
     }
     trend_change: Optional[str] = ""
-
-
-@attr.s(auto_attribs=True)
-class FunnelConfig:
-    configs: List[ModelConfig] = attr.Factory(list)
-
-    @classmethod
-    def collect_funnel_configs(cls, config_toml_path: str):
-        config_toml_path = Path(config_toml_path)
-        configs = toml.load(config_toml_path)
-
-        parsed_configs = []
-        for metric, segments in configs.items():
-            metric_name = metric
-            for segment_slug, segment_config in segments.items():
-                config = {
-                    "metric": metric_name,
-                    "slug": segment_slug,
-                    "segment": segment_config["segment"],
-                    "start_date": pd.to_datetime(segment_config["start_date"]),
-                }
-                if "trend_change" in segment_config.keys():
-                    config["trend_change"] = segment_config["trend_change"]
-
-                if "holidays" in segment_config.keys():
-                    if "use_country_holidays" in segment_config["holidays"].keys():
-                        config["use_country_holidays"] = segment_config["holidays"][
-                            "use_country_holidays"
-                        ]
-                    config["holidays"] = pd.concat(
-                        [
-                            getattr(holiday_collection, h)
-                            for h in segment_config["holidays"]["holidays"]
-                        ]
-                    )
-
-                if "regressors" in segment_config.keys():
-                    regressor_list = []
-                    for name, params in segment_config["regressors"].items():
-                        regressor_list.append(
-                            ProphetRegressor(
-                                name=name,
-                                start_date=(
-                                    params["start_date"]
-                                    if "start_date" in params.keys()
-                                    else None
-                                ),
-                                end_date=(
-                                    params["end_date"]
-                                    if "end_date" in params.keys()
-                                    else None
-                                ),
-                                prior_scale=params["prior_scale"],
-                                mode=params["mode"],
-                            )
-                        )
-                    config["regressors"] = regressor_list
-
-                if "parameters" in segment_config.keys():
-                    config["parameters"] = segment_config["parameters"]
-
-                if "cv_settings" in segment_config.keys():
-                    config["cv_settings"] = segment_config["cv_settings"]
-
-                if "changepoints" in segment_config.keys():
-                    config["changepoints"] = segment_config["changepoints"]["dates"]
-
-                parsed_configs.append(Config(**config))
-
-        return cls(config_toml_path, parsed_configs)
