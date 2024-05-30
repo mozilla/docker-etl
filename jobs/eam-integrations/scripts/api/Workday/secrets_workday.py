cb100de8eb9342e64c6d199ba99fefa98416fc3f
Julio Cezar Moscon <jcmoscon@gmail.com>
2024-05-30 09:49:50 -0400

    Fixed init and secret files

--- jobs/eam-integrations/scripts/api/Workday/secrets_workday.py
+++ jobs/eam-integrations/scripts/api/Workday/secrets_workday.py
@@ -7,8 +7,8 @@ config = {
         "username": os.environ.get("XMATTERS_INTEG_WORKDAY_USERNAME", ""),
         "password": os.environ.get("XMATTERS_INTEG_WORKDAY_PASSWORD", ""),
         "sites_url":  "https://wd2-impl-services1.workday.com/ccx/service/\
-            customreport2/vhr_mozilla1/ISU_RAAS/Mozilla_BusContSites?format=json",
+customreport2/vhr_mozilla1/ISU_RAAS/Mozilla_BusContSites?format=json",
         "people_url": "https://wd2-impl-services1.workday.com/ccx/service/\
-            customreport2/vhr_mozilla1/ISU_RAAS/Mozilla_BusContUsers?format=json",
+customreport2/vhr_mozilla1/ISU_RAAS/Mozilla_BusContUsers?format=json",
     },
 }
