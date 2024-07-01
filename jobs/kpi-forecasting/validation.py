fe3cc673c8438cf3fb314dc44c733a295ce8260d
Jared Snyder <jsnyder@mozilla.com>
2024-07-01 14:05:43 -0500

    feat: add class to do validation and module to run it

---
+++ jobs/kpi-forecasting/validation.py
@@ -0,0 +1,26 @@
+from kpi_forecasting.validator import Validator
+
+config_list_search = [
+    "search_forecasting_ad_clicks.yaml",
+    "search_forecasting_daily_active_users.yaml",
+    "search_forecasting_search_count.yaml",
+]
+search_output_name = "search_validation"
+
+config_list_kpi = ["dau_desktop.yaml", "dau_mobile.yaml"]
+kpi_output_name = "kpi_validation"
+
+
+def main() -> None:
+    search_validator = Validator(
+        config_list_search, "moz-fx-data-bq-data-science", "jsnyder", search_output_name
+    )
+    search_validator.write()
+    kpi_validator = Validator(
+        config_list_kpi, "moz-fx-data-bq-data-science", "jsnyder", kpi_output_name
+    )
+    kpi_validator.write()
+
+
+if __name__ == "__main__":
+    main()
