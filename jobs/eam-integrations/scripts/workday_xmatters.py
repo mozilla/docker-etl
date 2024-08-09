3773846de01f0dc0e2d6be933577f874a63a6c29
JCMOSCON1976 <167822375+JCMOSCON1976@users.noreply.github.com>
2024-08-09 18:39:45 -0400

    feat(code):Add max_limit parameter and more logs (#266)
    * feat(code):added max_limit parameter and more logs
    
    * feat(code):added limit parameter and logs
    
    ---------
    
    Co-authored-by: Julio Cezar Moscon <jcmoscon@gmail.com>

--- jobs/eam-integrations/scripts/workday_xmatters.py
+++ jobs/eam-integrations/scripts/workday_xmatters.py
@@ -123,9 +123,10 @@ def user_data_matches(wd_user, xm_user):
         return False
 
 
-def iterate_thru_wd_users(wd_users, xm_users, xm_sites):
+def iterate_thru_wd_users(wd_users, xm_users, xm_sites, limit):
     wd_users_seen = {}
     xm_add_users = []
+    num_changes = 0
     for user in wd_users:
         if "User_Email_Address" not in user:
             logger.info(
@@ -156,6 +157,7 @@ def iterate_thru_wd_users(wd_users, xm_users, xm_sites):
                 XMatters.update_user(
                     user, xm_users[user["User_Email_Address"]], xm_sites
                 )
+                num_changes +=1
             else:
                 logger.debug("%s good" % user["User_Email_Address"])
         else:
@@ -163,7 +165,12 @@ def iterate_thru_wd_users(wd_users, xm_users, xm_sites):
             # XMatters.add_user(user, xm_sites)
             xm_add_users.append(user)
             # time.sleep(5)
-
+         
+        if num_changes >= limit:
+            logger.info(f"Number of updated users:{num_changes}")
+            return wd_users_seen, xm_add_users
+    
+    logger.info(f"Number of updated users:{num_changes}")
     return wd_users_seen, xm_add_users
 
 
@@ -206,14 +213,15 @@ if __name__ == "__main__":
         type=str,
         default="info",
     )
+     
     parser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="force changes even if there are a lot",
-
+        "-m",
+        "--max_limit", 
+        action="store",
+        type=int,
+        help="limit the number of changes in Everfi",        
+        default=20
     )
-
     args = parser.parse_args()
 
     Util.set_up_logging(args.level)
@@ -224,62 +232,70 @@ if __name__ == "__main__":
 
     # get all sites in xmatters
     xm_sites, xm_sites_inactive = XMatters.get_all_sites()
+    
 
     # get all users from workday
     wd_users = Workday.get_users()
+    
 
     # get the new style (zipcodes) sites from the user list
     wd_sites = get_wd_sites_from_users(wd_users)
-
+    
+    logger.info(f"Number of XMatters sites: {len(xm_sites)}")
+    logger.info(f"Number of Workday sites: {len(wd_sites)}")
+    logger.info(f"Number of Workday users: {len(wd_users)}")
+                
     #  # get list of sites from workday users
     #  wd_sites = Workday.get_sites()
 
-    sites_percentage = len(xm_sites) / len(wd_sites)
-    if sites_percentage > 1.1 or sites_percentage < 0.9:
-        logger.critical(
-            "The number of sites in Workday vs XMatters is \
-                 different by more than 10%% (%.02f%%)."
-            % (abs(100 - sites_percentage * 100))
-        )
-        logger.critical("Stopping unless --force")
-        if not args.force:
-            exit(42)
+    # sites_percentage = len(xm_sites) / len(wd_sites)
+    # if sites_percentage > 1.1 or sites_percentage < 0.9:
+    #     logger.critical(
+    #         "The number of sites in Workday vs XMatters is \
+    #              different by more than 10%% (%.02f%%)."
+    #         % (abs(100 - sites_percentage * 100))
+    #     )
+    #     logger.critical("Stopping unless --force")
+    #     if not args.force:
+    #         exit(42)
 
     # add_task any sites in workday that aren't in xmatters to xmatters
-    xm_sites_in_wd = XMatters.add_new_sites(wd_sites, xm_sites, xm_sites_inactive)
+    xm_sites_in_wd = XMatters.add_new_sites(wd_sites, xm_sites, xm_sites_inactive, args.max_limit)
 
     # delete any sites NOT in workday that ARE in xmatters
-    XMatters.delete_sites(xm_sites, xm_sites_in_wd)
+    XMatters.delete_sites(xm_sites, xm_sites_in_wd, args.max_limit)
 
     # re-get all sites in xmatters
     xm_sites, xm_sites_inactive = XMatters.get_all_sites()
 
     # get all users from xmatters
     xm_users = XMatters.get_all_people()
-
-    users_percentage = len(xm_users) / len(wd_users)
-    if users_percentage > 1.1 or users_percentage < 0.9:
-        logger.critical(
-            "The number of users in Workday vs XMatters is \
-            different by more than 10%% (%.02f%%)."
-            % (abs(100 - users_percentage * 100))
-        )
-        logger.critical("Stopping unless --force")
-        if not args.force:
-            exit(42)
+    logger.info(f"Number of XMatters users: {len(xm_users)}")
+    # users_percentage = len(xm_users) / len(wd_users)
+    # if users_percentage > 1.1 or users_percentage < 0.9:
+    #     logger.critical(
+    #         "The number of users in Workday vs XMatters is \
+    #         different by more than 10%% (%.02f%%)."
+    #         % (abs(100 - users_percentage * 100))
+    #     )
+    #     logger.critical("Stopping unless --force")
+    #     if not args.force:
+    #         exit(42)
 
     # iterate thru users in workday:
     #   if not in xmatters, add_task to xmatters
     #   if data doesn't match xmatters, update xmatters
     #   mark-as-seen in xmatters
     users_seen_in_workday, xm_add_users = iterate_thru_wd_users(wd_users,
-                                                                xm_users, xm_sites)
+                                                                xm_users, xm_sites,
+                                                                args.max_limit)
 
     # iterate through xmatters users who aren't marked-as-seen
     #   remove from xmatters
-    XMatters.delete_users(xm_users, users_seen_in_workday)
+    XMatters.delete_users(xm_users, users_seen_in_workday,args.max_limit)
 
-    for user in xm_add_users:
+    for user in xm_add_users[:args.max_limit]:
         XMatters.add_user(user, xm_sites)
 
+    logger.info(f"Number of users added:{len(xm_add_users[:args.max_limit])}")
     logger.info("Finished.")
