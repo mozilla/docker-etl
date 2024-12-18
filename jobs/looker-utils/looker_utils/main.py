4831d96d39d3f1489491c071a12e833ebd8dcafb
Anna Scholtz <anna@scholtzan.net>
2024-12-18 11:19:01 -0800

    Format looker utils

--- jobs/looker-utils/looker_utils/main.py
+++ jobs/looker-utils/looker_utils/main.py
@@ -1,60 +1,10 @@
-import click
 import os
-import looker_sdk
-from looker_sdk import methods40 as methods
-from looker_sdk import models40 as models 
 from datetime import datetime, timedelta, timezone
 
-class LookerConnection:
+import click
+import looker_sdk
+from looker_sdk import methods40 as methods
 
-    def __init__(self, client_id, client_secret, url):
-
-        if url[-1] == '/':
-            self.url = url[:-1] + ':19999/api/3.0/'
-        else:
-            self.url = url + ':19999/api/3.0/'
-        self.headers = self.connect(client_id, client_secret)
-        
-    def connect(self, client_id, client_secret):
-        """Gets Access Token from Looker, setting token on LookerConnection"""
-
-        login = requests.post(
-            url=compose_url(self.url, 'login'),
-            data={'client_id': client_id, 'client_secret': client_secret})
-
-        try:
-            access_token = login.json()['access_token']
-            headers = {'Authorization': 'token {}'.format(access_token)}
-        except KeyError:
-            headers = None
-
-        return headers
-    
-    def _get(self, endpoint, endpointid=None, subendpoint=None, subendpointid=None):
-        
-        r = requests.get(
-            url=compose_url(self.url, endpoint, endpointid=endpointid, subendpoint=subendpoint, subendpointid=subendpointid),
-            headers=self.headers)
-       
-        return r
-
-    def _delete(self, endpoint, endpointid=None, subendpoint=None, subendpointid=None):
-
-        r = requests.delete(
-            url=compose_url(self.url, endpoint, endpointid=endpointid, subendpoint=subendpoint, subendpointid=subendpointid),
-            headers=self.headers)
-        
-        return r
-
-    def _patch(self, endpoint, endpointid=None, subendpoint=None, subendpointid=None, payload=None):
-
-        r = requests.patch(
-            url=compose_url(self.url, endpoint, endpointid=endpointid, subendpoint=subendpoint, subendpointid=subendpointid),
-            headers=self.headers,
-            json=payload)
-        
-        return r
-    
 
 def setup_sdk(client_id, client_secret, instance) -> methods.Looker40SDK:
     os.environ["LOOKERSDK_BASE_URL"] = instance
@@ -68,9 +18,18 @@ def setup_sdk(client_id, client_secret, instance) -> methods.Looker40SDK:
 
 
 @click.group()
-@click.option("--client_id", "--client-id", envvar="LOOKER_API_CLIENT_ID", required=True)
-@click.option("--client_secret", "--client-secret", envvar="LOOKER_API_CLIENT_SECRET", required=True)
-@click.option("--instance_uri", "--instance-uri", envvar="LOOKER_INSTANCE_URI", required=True)
+@click.option(
+    "--client_id", "--client-id", envvar="LOOKER_API_CLIENT_ID", required=True
+)
+@click.option(
+    "--client_secret",
+    "--client-secret",
+    envvar="LOOKER_API_CLIENT_SECRET",
+    required=True,
+)
+@click.option(
+    "--instance_uri", "--instance-uri", envvar="LOOKER_INSTANCE_URI", required=True
+)
 @click.pass_context
 def cli(ctx: dict, client_id: str, client_secret: str, instance_uri: str):
     sdk = setup_sdk(client_id, client_secret, instance_uri)
@@ -79,9 +38,32 @@ def cli(ctx: dict, client_id: str, client_secret: str, instance_uri: str):
 
 
 @cli.command()
-@click.option("--project", help="Looker project name", multiple=True, default=["spoke-default", "looker-hub"])
-@click.option("--n_days", "--n-days", help="Delete branches that haven't been updated within the last n days", default=180)
-@click.option("--exclude", multiple=True, help="Branches to exclude from deletion", default=["main", "master"])
+@click.option(
+    "--project",
+    help="Looker project name",
+    multiple=True,
+    default=["spoke-default", "looker-hub"],
+)
+@click.option(
+    "--n_days",
+    "--n-days",
+    help="Delete branches that haven't been updated within the last n days",
+    default=180,
+)
+@click.option(
+    "--exclude",
+    multiple=True,
+    help="Branches to exclude from deletion",
+    default=[
+        "main",
+        "master",
+        "main-dev",
+        "prod",
+        "main-validation",
+        "main-stage",
+        "base",
+    ],
+)
 @click.pass_context
 def delete_branches(ctx, project, n_days, exclude):
     sdk = ctx.obj["SDK"]
@@ -93,21 +75,17 @@ def delete_branches(ctx, project, n_days, exclude):
         for branch in branches:
             commit_date = datetime.fromtimestamp(branch.commit_at, timezone.utc)
 
-            if commit_date < date_cutoff and not branch.name.startswith('dev') and branch.name not in exclude:
-                print(f"Deleting branch {branch.name}, last commit on {commit_date}")
-
-
-    # branch_count = 0
-
-    # for branch in branches.json():
-    #     commit_date = datetime.utcfromtimestamp(branch['commit_at'])
-    #     branch_name = branch['name']
-    #     if commit_date < date_cutoff and not branch_name.startswith('dev') and branch_name != 'master':
-    #         print("Deleting branch '{}', last commit on {}".format(branch_name, commit_date.strftime('%Y-%m-%d')))
-    #         delete_branch = conn._delete('projects',id,'git_branch',branch_name)
-    #         branch_count += 1
-            
-    # print(branch_count)
+            if (
+                commit_date < date_cutoff
+                and not branch.name.startswith("dev")
+                and branch.name not in exclude
+            ):
+                print(
+                    f"Deleting branch {branch.name} {lookml_project}, last commit on {commit_date}"
+                )
+                sdk.delete_git_branch(
+                    project_id=lookml_project, branch_name=branch.name
+                )
 
 
 if __name__ == "__main__":
--- jobs/looker-utils/setup.py
+++ jobs/looker-utils/setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 readme = open("README.md").read()
 
 setup(
-    name="docker-etl-job",  # TODO: change placeholder name
+    name="looker-utils",
     version="0.1.0",
     author="Mozilla Corporation",
-    packages=find_packages(include=["docker_etl"]), # TODO: change placeholder name
+    packages=find_packages(include=["looker_utils"]),
     long_description=readme,
     include_package_data=True,
     license="MPL 2.0",
