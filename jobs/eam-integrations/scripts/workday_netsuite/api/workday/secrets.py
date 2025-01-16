775f72bd26d313c4bfa94efc53c985f3e00c0164
Julio Cezar Moscon <jcmoscon@gmail.com>
2025-01-16 17:56:56 -0500

    fix(code): Change Secret Names

--- jobs/eam-integrations/scripts/workday_netsuite/api/workday/secrets.py
+++ jobs/eam-integrations/scripts/workday_netsuite/api/workday/secrets.py
@@ -1,12 +1,12 @@
 import os
 
 config = {
-    "link": os.environ.get("wd_listing_of_workers_link", ""),
-    "username": os.environ.get("WD_USERNAME", ""),
-    "password": os.environ.get("WD_PASS", ""),
+    "link": os.environ.get("NETSUITE_INTEG_WORKDAY_LISTING_OF_WORKERS_LINK", ""),
+    "username": os.environ.get("NETSUITE_INTEG_WORKDAY_USERNAME", ""),
+    "password": os.environ.get("NETSUITE_INTEG_WORKDAY_PASSWORD", ""),
     "links": {
-        "wd_listing_of_workers_link": os.environ.get("wd_listing_of_workers_link", ""),
-        "wd_international_transfers_link": os.environ.get("wd_international_transfers_link", ""),
+        "wd_listing_of_workers_link": os.environ.get("NETSUITE_INTEG_WORKDAY_LISTING_OF_WORKERS_LINK", ""),
+        "wd_international_transfers_link": os.environ.get("NETSUITE_INTEG_WORKDAY_INTERNATIONAL_TRANSFER_LINK", ""),
     },
     "timeout": 30,
 }
