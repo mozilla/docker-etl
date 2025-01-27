3bae62edadf072be57d3ef9082b4b829a9637b8c
James Graham <james@hoppipolla.co.uk>
2025-01-27 17:24:02 +0000

    Use uv to runs tests if it's available
    This significantly increases the performance of setting up the test environment.

--- jobs/webcompat-kb/test.sh
+++ jobs/webcompat-kb/test.sh
@@ -1,15 +1,23 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 
 set -ex
 
-if [ ! -d "_venv" ]; then
-  python3 -m _venv
+if [ -x "$(command -v uv)" ]; then
+    uv sync --extra=test
+    uv run mypy -m webcompat_kb
+    uv run pytest --ruff --ruff-format .
+else
+    if [ ! -d "_venv" ]; then
+        python3 -m _venv
+    fi
+    source _venv/bin/activate
+
+    pip install -e .
+    pip install .[test]
+
+    mypy -m webcompat_kb
+    pytest --ruff --ruff-format .
 fi
-source _venv/bin/activate
 
-pip install -e .
-pip install .[test]
 
-mypy -m webcompat_kb
-pytest --ruff --ruff-format .
 
