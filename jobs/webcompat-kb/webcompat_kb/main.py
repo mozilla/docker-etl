657d57f8895545882d10d951f215d8b55e92842c
James Graham <james@hoppipolla.co.uk>
2025-04-02 17:51:05 +0100

    Add --pdb flag
    This is for local development and drops into a debugger if there's an
    unhandled exception.

--- jobs/webcompat-kb/webcompat_kb/main.py
+++ jobs/webcompat-kb/webcompat_kb/main.py
@@ -33,6 +33,10 @@ def get_parser() -> argparse.ArgumentParser:
         help="Don't write updates to BigQuery",
     )
 
+    parser.add_argument(
+        "--pdb", action="store_true", help="Drop into debugger on execption"
+    )
+
     for job_cls in ALL_JOBS.values():
         job_cls.add_arguments(parser)
 
@@ -79,19 +83,32 @@ def main() -> None:
 
     parser = get_parser()
     args = parser.parse_args()
-    logging.getLogger().setLevel(logging.getLevelNamesMapping()[args.log_level.upper()])
-    set_default_args(parser, args)
 
-    jobs = {job_name: ALL_JOBS[job_name]() for job_name in args.jobs}
+    try:
+        logging.getLogger().setLevel(
+            logging.getLevelNamesMapping()[args.log_level.upper()]
+        )
+        set_default_args(parser, args)
 
-    for job_name, job in jobs.items():
-        job.set_default_args(parser, args)
+        jobs = {job_name: ALL_JOBS[job_name]() for job_name in args.jobs}
 
-    client = get_client(args.bq_project_id)
+        for job_name, job in jobs.items():
+            job.set_default_args(parser, args)
 
-    for job_name, job in jobs.items():
-        logging.info(f"Running job {job_name}")
-        job.main(client, args)
+        client = get_client(args.bq_project_id)
+
+        for job_name, job in jobs.items():
+            logging.info(f"Running job {job_name}")
+            job.main(client, args)
+    except Exception:
+        if args.pdb:
+            import pdb
+            import traceback
+
+            traceback.print_exc()
+            pdb.post_mortem()
+        else:
+            raise
 
 
 if __name__ == "__main__":
