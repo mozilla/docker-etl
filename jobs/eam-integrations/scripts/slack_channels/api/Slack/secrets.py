14ba1445b793c53a10c7ff1e20a7f970b7ed3f4a
Julio Cezar Moscon <jmoscon@mozilla.com>
2025-05-21 16:57:41 -0400

    fix(code): Add new env variable

--- jobs/eam-integrations/scripts/slack_channels/api/Slack/secrets.py
+++ jobs/eam-integrations/scripts/slack_channels/api/Slack/secrets.py
@@ -3,5 +3,5 @@ import os
 config = {
     "slack_host": "https://slack.com/",
     "slack_token": os.environ.get("SLACK_CHANNEL_TOKEN", ""),    
-    "slack_service_account":'UCW50U0FL'
+    "slack_service_account":os.environ.get("slack_service_account", "")
 }
