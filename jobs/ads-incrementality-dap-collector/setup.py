613397601619ce7ff30ed3c5fc5985090daf4a55
Masha Lifshin <mlifshin@mozilla.com>
2025-08-28 17:52:46 -0700

    WIP tests and add some hopefully helpful type hints

--- jobs/ads-incrementality-dap-collector/setup.py
+++ jobs/ads-incrementality-dap-collector/setup.py
@@ -8,7 +8,7 @@ setup(
     name="ads-incrementality-dap-collector",
     version="0.1.0",
     author="gleonard@mozilla.com",
-    packages=find_packages(include=["ads-incrementality-dap-collector"]),
+    packages=find_packages(include=["ads_incrementality_dap_collector"]),
     long_description=readme,
     include_package_data=True,
     license="MPL 2.0",
