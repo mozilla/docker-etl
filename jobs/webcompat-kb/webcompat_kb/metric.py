7f6530f5d0f27c56a188147872b27836b9a5b0a9
James Graham <james@hoppipolla.co.uk>
2025-09-03 09:39:37 +0100

    Add a command for backfilling the metric scores
    When we add a new metric type we generally want to backfill the
    results we would have got at earlier dates based on the current
    data in bugzilla. This won't always match the actual results on those
    dates because we're looking at current bug information rather than the
    bug as it would have appeared on that date, but it's generally good
    enough to provide some historical context.
    
    For this reason add a metric-backfill command that is intended to be
    used interactively with a `--metric-backfill-metric` argument to
    specify the metric that should be backfilled.

--- jobs/webcompat-kb/webcompat_kb/metric.py
+++ jobs/webcompat-kb/webcompat_kb/metric.py
@@ -207,6 +207,62 @@ INSERT `{bq_dataset_id}.webcompat_topline_metric_daily`
         logging.info(f"Would insert {list(result)[0]}")
 
 
+def backfill_metric_daily(
+    client: BigQuery, bq_dataset_id: str, write: bool, metric_name: str
+) -> None:
+    daily_metric_types = [
+        metric_type for metric_type in metric_types if "daily" in metric_type.contexts
+    ]
+
+    metric = None
+    for metric in metrics:
+        if metric.name == metric_name:
+            break
+    else:
+        raise ValueError(f"Metric named {metric_name} not found")
+
+    select_fields = []
+    field_names = []
+    conditions = []
+    for metric_type in daily_metric_types:
+        field_name = f"{metric_type.name}_{metric.name}"
+        field_names.append(field_name)
+        select_fields.append(
+            f"{metric_type.agg_function('bugs', metric)} AS {field_name}"
+        )
+        conditions.append(f"metric_daily.{field_name} IS NULL")
+    select_query = f"""
+SELECT
+  date,
+  {",\n  ".join(select_fields)}
+FROM
+  `{bq_dataset_id}.scored_site_reports` AS bugs
+  JOIN `{bq_dataset_id}.webcompat_topline_metric_daily` as metric_daily
+ON
+  DATE(bugs.creation_time) <= metric_daily.date
+  AND IF (bugs.resolved_time IS NOT NULL, DATE(bugs.resolved_time) >= date, TRUE)
+WHERE
+  {metric.site_reports_field} AND {" AND ".join(conditions)}
+GROUP BY
+  date
+ORDER BY date"""
+
+    update_query = f"""
+UPDATE `{bq_dataset_id}.webcompat_topline_metric_daily` AS metric_daily
+SET
+  {",\n  ".join(f"metric_daily.{field_name}=new_data.{field_name}" for field_name in field_names)}
+FROM ({select_query}) AS new_data
+WHERE new_data.date = metric_daily.date
+"""
+
+    if write:
+        result = client.query(update_query)
+    else:
+        logging.info(f"Would run query:\n{update_query}")
+        result = client.query(select_query)
+        logging.info(f"Would set {list(result)}")
+
+
 class MetricJob(EtlJob):
     name = "metric"
 
@@ -219,3 +275,28 @@ class MetricJob(EtlJob):
     def main(self, client: BigQuery, args: argparse.Namespace) -> None:
         update_metric_history(client, args.bq_kb_dataset, args.write)
         update_metric_daily(client, args.bq_kb_dataset, args.write)
+
+
+class MetricBackfillJob(EtlJob):
+    name = "metric-backfill"
+    default = False
+
+    @classmethod
+    def add_arguments(cls, parser: argparse.ArgumentParser) -> None:
+        group = parser.add_argument_group(
+            title="Metric Backfill", description="metric-backfill arguments"
+        )
+        group.add_argument(
+            "--metric-backfill-metric", help="Name of the metric to backfill"
+        )
+
+    def required_args(self) -> set[str | tuple[str, str]]:
+        return {"bq_kb_dataset", "metric_backfill_metric"}
+
+    def default_dataset(self, args: argparse.Namespace) -> str:
+        return args.bq_kb_dataset
+
+    def main(self, client: BigQuery, args: argparse.Namespace) -> None:
+        backfill_metric_daily(
+            client, args.bq_kb_dataset, args.write, args.metric_backfill_metric
+        )
