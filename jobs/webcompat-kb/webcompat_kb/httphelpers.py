af198fa94743b927cf8cf1c9e894ae4ea8393772
James Graham <james@hoppipolla.co.uk>
2025-09-18 11:59:12 +0100

    Add import of Interop issues into webcompat_kb
    Currently this is just a simple import that looks for issues labelled
    with either `focus-area-proposal` or `investigation-proposal` and
    stores a row with the issue number and name of the proposal, along
    with any web-features or bugzilla issues linked in either the original
    proposal or the web-features bot comment.
    
    Later we will probably want some way to handle proposals being closed
    / accepted / etc. but that can be a followup.
    
    Due to GitHub rate limits, this requires a GitHub token to run correctly.

--- jobs/webcompat-kb/webcompat_kb/httphelpers.py
+++ jobs/webcompat-kb/webcompat_kb/httphelpers.py
@@ -1,3 +1,5 @@
+import re
+from dataclasses import dataclass
 from typing import Mapping, Optional, Sequence
 
 import httpx
@@ -9,3 +11,37 @@ def get_json(url: str, headers: Optional[Mapping[str, str]] = None) -> Json:
     resp = httpx.get(url, headers=headers, follow_redirects=True)
     resp.raise_for_status()
     return resp.json()
+
+
+@dataclass
+class LinkHeader:
+    prev: Optional[str] = None
+    next: Optional[str] = None
+    first: Optional[str] = None
+    last: Optional[str] = None
+
+
+def parse_link_header(link: Optional[str]) -> LinkHeader:
+    rv = LinkHeader()
+    if link is None:
+        return rv
+
+    rel_link = re.compile(r"""<(?P<url>[^>]+)>\s*;\s*rel=["']?(?P<rel>\w+)["']?\s*,?""")
+
+    for item in rel_link.finditer(link):
+        if item["rel"] in {"prev", "next", "first", "last"}:
+            setattr(rv, item["rel"], item["url"])
+
+    return rv
+
+
+def get_paginated_json(url: str, headers: Optional[Mapping[str, str]] = None) -> Sequence[Json]:
+    data = []
+    next_url: Optional[str] = url
+    while next_url is not None:
+        resp = httpx.get(next_url, headers=headers, follow_redirects=True)
+        resp.raise_for_status()
+        data.extend(resp.json())
+        links = parse_link_header(resp.headers.get("link"))
+        next_url = links.next
+    return data
