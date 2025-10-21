484c7ce6b389aa582fbb894054f89b0759733fa7
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-10-21 15:31:10 -0500

    feat(DENG-9504): Create docker ETL job for pulling release notes (#410)
    * feat(DENG-9504): Create docker ETL job for pulling Chrome release notes
    
    * Add project ID and switch from get_bucket to bucket
    
    * Add timeout
    
    * Add selenium
    
    * Update main.py

---
+++ jobs/release_scraping/release_scraping/main.py
@@ -0,0 +1,71 @@
+# Load libraries
+import requests
+from google.cloud import storage
+from datetime import datetime
+from bs4 import BeautifulSoup
+from argparse import ArgumentParser
+
+# Website with chrome release data
+CHROME_RELEASES_URL = "https://developer.chrome.com/release-notes"
+
+# Website with AI in Chrome information
+AI_IN_CHROME_URL = "https://developer.chrome.com/docs/ai"
+
+# Define where to write the data in GCS
+GCS_BUCKET = "gs://moz-fx-data-prod-external-data/"
+BUCKET_NO_GS = "moz-fx-data-prod-external-data"
+RESULTS_FPATH_1 = "MARKET_RESEARCH/SCRAPED_INFO/ChromeReleaseNotes/WebScraping_"
+RESULTS_FPATH_2 = "MARKET_RESEARCH/SCRAPED_INFO/ChromeAI/WebScraping_"
+TIMEOUT_IN_SECONDS = 20
+
+
+def get_latest_chrome_release_url(CHROME_RELEASES_MAIN_PAGE):
+    """Get the URL for the latest chrome release"""
+    # TODO Scrape CHROME_RELEASES_MAIN_PAGE to get latest release instead of hardcoding
+    latest_chrome_release_url = "https://developer.chrome.com/release-notes/141?hl=en"
+    return latest_chrome_release_url
+
+
+def main():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--date", required=True)
+    args = parser.parse_args()
+
+    # Get DAG logical date
+    logical_dag_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+    logical_dag_date_string = logical_dag_date.strftime("%Y%m%d")
+
+    # Make final output filepaths using DAG date
+    final_output_fpath1 = RESULTS_FPATH_1 + logical_dag_date_string + ".txt"
+    final_output_fpath2 = RESULTS_FPATH_2 + logical_dag_date_string + ".txt"
+
+    # Get latest chrome release URL
+    latest_chrome_release_url = get_latest_chrome_release_url(CHROME_RELEASES_URL)
+
+    # Get latest Chrome release info
+    chrome_release_url_response = requests.get(
+        latest_chrome_release_url, timeout=TIMEOUT_IN_SECONDS
+    )
+    soup = BeautifulSoup(chrome_release_url_response.text, "html.parser")
+    final_output_1 = soup.get_text(separator="\n", strip=True)
+
+    # Get info about AI on Chrome
+    chrome_ai_url_response = requests.get(AI_IN_CHROME_URL, timeout=TIMEOUT_IN_SECONDS)
+    soup = BeautifulSoup(chrome_ai_url_response.text, "html.parser")
+    final_output_2 = soup.get_text(separator="\n", strip=True)
+
+    # Open up a client to GCS
+    client = storage.Client(project="moz-fx-data-shared-prod")
+    bucket = client.bucket(BUCKET_NO_GS)
+
+    blob = bucket.blob(final_output_fpath1)
+    blob.upload_from_string(final_output_1)
+    print(f"Summary uploaded to gs://{BUCKET_NO_GS}/{final_output_fpath1}")
+
+    blob2 = bucket.blob(final_output_fpath2)
+    blob2.upload_from_string(final_output_2)
+    print(f"Summary uploaded to gs://{BUCKET_NO_GS}/{final_output_fpath1}")
+
+
+if __name__ == "__main__":
+    main()
