f5376cd7020d79312a344d3f72fe2805b4299aaf
James Graham <james@hoppipolla.co.uk>
2025-11-07 14:09:30 +0000

    Support adding table columns.
    This allows adding new columns to an existing table, diffing the new
    list of columns with the old ones, and adding any that are missing.
    
    It doesn't support removing columns or otherwise changing the schema
    definition.

--- jobs/webcompat-kb/tests/test_bugzilla.py
+++ jobs/webcompat-kb/tests/test_bugzilla.py
@@ -1,13 +1,12 @@
 import tempfile
 from collections import defaultdict
 from datetime import datetime, timezone
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from typing import Any, Iterable, Mapping
 
 import pytest
 import bugdantic.bugzilla
 
-from webcompat_kb.bqhelpers import get_client, BigQuery
 from webcompat_kb.bugzilla import (
     Bug,
     BugHistoryChange,
@@ -1193,20 +1192,7 @@ KEYWORDS_AND_STATUS = to_history(
 )
 
 
-@pytest.fixture(scope="module")
-@patch("webcompat_kb.bqhelpers.google.auth.default")
-@patch("webcompat_kb.bqhelpers.bigquery.Client")
-def bq_client(mock_bq, mock_auth_default):
-    mock_credentials = Mock()
-    mock_project_id = "placeholder_id"
-    mock_auth_default.return_value = (mock_credentials, mock_project_id)
-    mock_bq.return_value = Mock()
-
-    mock_bq.return_value = Mock()
-    return BigQuery(get_client(mock_project_id), "test_dataset", True)
-
-
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def history_updater(bq_client):
     return BugHistoryUpdater(bq_client, None)
 
