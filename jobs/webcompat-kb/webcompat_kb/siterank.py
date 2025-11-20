e305b0a9b873b6120b461a8b5dab7fa9911ed161
jgraham <james@hoppipolla.co.uk>
2025-11-20 16:14:08 +0000

    Port site-ranks job to new infrastructure (#447)
    Co-authored-by: Ksenia <kberezina@mozilla.com>

--- jobs/webcompat-kb/webcompat_kb/siterank.py
+++ jobs/webcompat-kb/webcompat_kb/siterank.py
@@ -1,42 +1,21 @@
 import argparse
 import csv
 import logging
-from dataclasses import dataclass
 from datetime import datetime, UTC
-from typing import Iterator, Self
+from typing import Iterator
 
 import httpx
 from google.cloud import bigquery
 
 from .base import Context, EtlJob, dataset_arg
-from .bqhelpers import BigQuery, Json, RangePartition, get_client
+from .bqhelpers import BigQuery, Json, TableSchema
 from .httphelpers import get_json
 from .projectdata import Project
 
 
-@dataclass
-class Config:
-    bq_project: str
-    bq_crux_dataset: str
-    bq_tranco_dataset: str
-    write: bool
-
-    @classmethod
-    def from_args(cls, args: argparse.Namespace) -> Self:
-        return cls(
-            bq_project=args.bq_project_id,
-            bq_crux_dataset=args.bq_crux_dataset,
-            bq_tranco_dataset=args.bq_tranco_dataset,
-            write=args.write,
-        )
-
-
-def get_last_import(client: BigQuery, config: Config) -> int:
-    query = f"""
-SELECT
-  yyyymm
-FROM
-  `{config.bq_crux_dataset}.import_runs`
+def get_latest_yyyymm(client: BigQuery, table: TableSchema) -> int:
+    query = f"""SELECT yyyymm
+FROM `{table}`
 ORDER BY yyyymm DESC
 LIMIT 1
 """
@@ -44,22 +23,9 @@ LIMIT 1
     try:
         result = list(client.query(query))
     except Exception:
+        # Table doesn't exist
         return 0
 
-    if not result:
-        return 0
-
-    return result[0]["yyyymm"]
-
-
-def get_latest_tranco_dataset(client: BigQuery, table: bigquery.Table) -> int:
-    query = rf"""SELECT yyyymm
-FROM `{table.table_id}`
-ORDER BY yyyymm DESC
-LIMIT 1
-"""
-
-    result = list(client.query(query))
     if len(result) != 1:
         return 0
 
@@ -85,28 +51,19 @@ LIMIT 1
     return result[0]["crux_date"]
 
 
-def update_crux_data(client: BigQuery, config: Config, date: int) -> None:
-    query = f"""
-SELECT yyyymm, origin, country_code, experimental.popularity.rank as rank from `chrome-ux-report.experimental.country` WHERE yyyymm = {date}
+def update_crux_data(project: Project, client: BigQuery, yyyymm: int) -> None:
+    table = project["crux_imported"]["origin_ranks"].table()
+    query = """
+SELECT yyyymm, origin, country_code, experimental.popularity.rank as rank from `chrome-ux-report.experimental.country` WHERE yyyymm = @yyyymm
 UNION ALL
-SELECT yyyymm, origin, "global" as country_code, experimental.popularity.rank as rank from `chrome-ux-report.experimental.global` WHERE yyyymm = {date}
+SELECT yyyymm, origin, "global" as country_code, experimental.popularity.rank as rank from `chrome-ux-report.experimental.global` WHERE yyyymm = @yyyymm
 """
-    schema = [
-        bigquery.SchemaField("yyyymm", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("origin", "STRING", mode="REQUIRED"),
-        bigquery.SchemaField("country_code", "STRING", mode="REQUIRED", max_length=8),
-        bigquery.SchemaField("rank", "INTEGER", mode="REQUIRED"),
-    ]
-
     logging.info("Updating CrUX data")
-    client.ensure_table(
-        "origin_ranks", schema, partition=RangePartition("yyyymm", 201701, 202501)
-    )
     client.insert_query(
-        "origin_ranks",
+        table,
         ["yyyymm", "origin", "country_code", "rank"],
         query,
-        dataset_id=config.bq_crux_dataset,
+        parameters=[bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)],
     )
 
 
@@ -123,55 +80,40 @@ def get_tranco_data() -> Iterator[tuple[int, str]]:
         yield int(row[0]), row[1]
 
 
-def update_tranco_data(
-    client: BigQuery, config: Config, table: bigquery.Table, yyyymm: int
-) -> None:
+def update_tranco_data(client: BigQuery, table: TableSchema, yyyymm: int) -> None:
     rows: list[dict[str, Json]] = [
         {"yyyymm": yyyymm, "rank": rank, "host": host}
         for rank, host in get_tranco_data()
     ]
-    if config.write:
-        client.query(
-            f"DELETE FROM `{table.table_id}` WHERE yyyymm = @yyyymm",
-            parameters=[bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)],
-        )
+    client.delete_query(
+        table,
+        condition="yyyymm = @yyyymm",
+        parameters=[bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)],
+    )
     client.write_table(table, table.schema, rows, overwrite=False)
 
 
-def update_sightline_data(client: BigQuery, config: Config, yyyymm: int) -> None:
+def update_sightline_data(project: Project, client: BigQuery, yyyymm: int) -> None:
+    origin_ranks_table = project["crux_imported"]["origin_ranks"].table()
+    sightline_top_1000_table = project["crux_imported"]["sightline_top_1000"].table()
     query = f"""
 SELECT
   DISTINCT yyyymm,
   NET.HOST(origin) AS host
 FROM
-  `{config.bq_crux_dataset}.origin_ranks` AS crux_ranks
+  `{origin_ranks_table}` AS crux_ranks
 JOIN UNNEST(["global", "us", "fr", "de", "es", "it", "mx"]) as country_code USING(country_code)
 WHERE
   crux_ranks.rank = 1000
   AND crux_ranks.yyyymm = {yyyymm}"""
 
-    schema = [
-        bigquery.SchemaField("yyyymm", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("host", "STRING"),
-    ]
-
     logging.info("Updating sightline data")
-    client.ensure_table(
-        "sightline_top_1000", schema, partition=RangePartition("yyyymm", 201701, 202501)
-    )
-    client.insert_query(
-        "sightline_top_1000",
-        ["yyyymm", "host"],
-        query,
-        dataset_id=config.bq_crux_dataset,
-    )
+    client.insert_query(sightline_top_1000_table, ["yyyymm", "host"], query)
 
 
-def update_min_rank_data(
-    project: Project, client: BigQuery, config: Config, yyyymm: int
-) -> None:
+def update_min_rank_data(project: Project, client: BigQuery, yyyymm: int) -> None:
     rank_columns = project.data.rank_dfns
-    column_names = [f"{item.name}" for item in rank_columns]
+    column_names = ["yyyymm", "host"] + [f"{item.name}" for item in rank_columns]
 
     crux_columns = ",\n    ".join(
         f"MIN(IF({column.crux_condition}, crux_ranks.rank, NULL)) as {column.name}"
@@ -209,7 +151,7 @@ WITH
   END
     AS rank_bucket
   FROM
-    `moz-fx-dev-dschubert-wckb.tranco_imported.top_1M_subdomains`
+    `{project["tranco_imported"]["top_1M_subdomains"]}`
   WHERE
     yyyymm = @yyyymm)
 
@@ -224,56 +166,29 @@ FULL OUTER JOIN
 USING
   (host)
 """
-    schema = [
-        bigquery.SchemaField("yyyymm", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("host", "STRING", mode="REQUIRED"),
-    ]
-    schema.extend(
-        bigquery.SchemaField(column.name, "INTEGER") for column in rank_columns
-    )
+    host_min_ranks_table = project["crux_imported"]["host_min_ranks"].table()
     parameters = [bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)]
     logging.info("Updating host_min_ranks data")
-    table = client.ensure_table(
-        "host_min_ranks",
-        schema,
-        partition=RangePartition("yyyymm", 201701, 202501),
+    client.delete_query(
+        host_min_ranks_table,
+        condition="yyyymm = @yyyymm",
+        parameters=parameters,
     )
-    if config.write:
-        client.query(
-            f"DELETE FROM `{table.table_id}` WHERE yyyymm = @yyyymm",
-            parameters=parameters,
-        )
     client.insert_query(
-        "host_min_ranks",
+        host_min_ranks_table,
         column_names,
         query,
-        dataset_id=config.bq_crux_dataset,
         parameters=parameters,
     )
 
 
-def update_import_date(
-    client: BigQuery, config: Config, run_at: datetime, data_yyyymm: int
-) -> None:
-    if not config.write:
-        return
-
-    formatted_time = run_at.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-    rows_to_insert = [
-        {
-            "run_at": formatted_time,
-            "yyyymm": data_yyyymm,
-        },
-    ]
-    logging.info("Updating last run date")
-
-    schema = [
-        bigquery.SchemaField("run_at", "TIMESTAMP", mode="REQUIRED"),
-        bigquery.SchemaField("yyyymm", "INTEGER", mode="REQUIRED"),
-    ]
-    runs_table = client.ensure_table("import_runs", schema)
-    client.insert_rows(runs_table, rows_to_insert)
+def record_update(client: BigQuery, table: TableSchema, yyyymm: int) -> None:
+    client.insert_query(
+        table,
+        columns=[item.name for item in table.fields],
+        query="SELECT @yyyymm, CURRENT_TIMESTAMP()",
+        parameters=[bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)],
+    )
 
 
 def get_previous_month_yyyymm(date: datetime) -> int:
@@ -288,7 +203,7 @@ def get_previous_month_yyyymm(date: datetime) -> int:
 
 
 def update_crux(
-    client: BigQuery, config: Config, last_import_yyyymm: int
+    project: Project, client: BigQuery, last_import_yyyymm: int
 ) -> tuple[int, bool]:
     latest_yyyymm = get_latest_crux_dataset(client)
     logging.debug(f"Latest CrUX data is {latest_yyyymm}")
@@ -297,27 +212,20 @@ def update_crux(
         logging.info("No new CrUX data available")
         return latest_yyyymm, False
 
-    update_crux_data(client, config, latest_yyyymm)
+    update_crux_data(project, client, latest_yyyymm)
     return latest_yyyymm, True
 
 
 def update_tranco(
-    client: BigQuery, config: Config, latest_crux_yyyymm: int, force_update: bool
+    project: Project, client: BigQuery, latest_crux_yyyymm: int, force_update: bool
 ) -> bool:
-    schema = [
-        bigquery.SchemaField("yyyymm", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("rank", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("host", "STRING", mode="REQUIRED"),
-    ]
-    subdomains_table = client.ensure_table(
-        "top_1M_subdomains", schema, partition=RangePartition("yyyymm", 201701, 202501)
-    )
-    last_import_yyyymm = get_latest_tranco_dataset(client, subdomains_table)
+    subdomains_table = project["tranco_imported"]["top_1M_subdomains"].table()
+    last_import_yyyymm = get_latest_yyyymm(client, subdomains_table)
 
     if last_import_yyyymm >= latest_crux_yyyymm and not force_update:
         return False
 
-    update_tranco_data(client, config, subdomains_table, latest_crux_yyyymm)
+    update_tranco_data(client, subdomains_table, latest_crux_yyyymm)
     return True
 
 
@@ -356,19 +264,16 @@ class SiteRanksJob(EtlJob):
         return context.args.bq_crux_dataset
 
     def main(self, context: Context) -> None:
+        project = context.project
+        client = context.bq_client
         run_at = datetime.now(UTC)
-        config = Config.from_args(context.args)
-        client_crux = context.bq_client
-        client_tranco = BigQuery(
-            get_client(context.args.bq_project_id),
-            context.args.bq_tranco_dataset,
-            write=context.config.write,
-        )
 
         last_month_yyyymm = get_previous_month_yyyymm(run_at)
         logging.debug(f"Last month was {last_month_yyyymm}")
 
-        last_import_yyyymm = get_last_import(client_crux, config)
+        import_runs_table = project["crux_imported"]["import_runs"].table()
+
+        last_import_yyyymm = get_latest_yyyymm(client, import_runs_table)
         logging.debug(f"Last site-ranks import was {last_import_yyyymm}")
 
         if (
@@ -378,39 +283,37 @@ class SiteRanksJob(EtlJob):
             logging.info("Site-ranks data is up to date")
             return
 
-        latest_yyyymm, have_new_crux = update_crux(
-            client_crux, config, last_import_yyyymm
-        )
+        latest_yyyymm, have_new_crux = update_crux(project, client, last_import_yyyymm)
         if have_new_crux or context.args.site_ranks_force_tranco_update:
             update_tranco(
-                client_tranco,
-                config,
+                project,
+                client,
                 latest_yyyymm,
                 context.args.site_ranks_force_tranco_update,
             )
 
         if have_new_crux:
-            update_sightline_data(client_crux, config, latest_yyyymm)
+            update_sightline_data(project, client, latest_yyyymm)
         if have_new_crux or context.args.site_ranks_force_host_min_ranks_update:
-            update_min_rank_data(context.project, client_crux, config, latest_yyyymm)
+            update_min_rank_data(project, client, latest_yyyymm)
         if have_new_crux:
-            update_import_date(client_crux, config, run_at, latest_yyyymm)
+            record_update(client, import_runs_table, latest_yyyymm)
 
 
-def check_yyyymm(client: BigQuery, config: Config, yyyymm: int) -> bool:
+def check_yyyymm(project: Project, client: BigQuery, yyyymm: int) -> bool:
     query = f"""
 SELECT EXISTS (
-  SELECT 1 FROM `{config.bq_crux_dataset}.host_min_ranks` WHERE yyyymm = @yyyymm) as has_yyyymm
+  SELECT 1 FROM `{project["crux_imported"]["host_min_ranks"]}` WHERE yyyymm = @yyyymm) as has_yyyymm
 """
     parameters = [bigquery.ScalarQueryParameter("yyyymm", "INTEGER", yyyymm)]
     result = list(client.query(query, parameters=parameters))[0]
     return result.has_yyyymm
 
 
-def create_new_routine(client: BigQuery, config: Config, yyyymm: int) -> str:
+def create_new_routine(client: BigQuery, yyyymm: int) -> str:
     new_name = f"{client.project_id}.{client.default_dataset_id}.WEBCOMPAT_METRIC_YYYYMM_{yyyymm}"
     query = f"CREATE OR REPLACE FUNCTION `{new_name}`() RETURNS INT64 AS ({yyyymm});"
-    if config.write:
+    if client.write:
         logging.info(f"Creating function {new_name}")
         client.query(query)
     else:
@@ -419,7 +322,7 @@ def create_new_routine(client: BigQuery, config: Config, yyyymm: int) -> str:
 
 
 def create_new_scored_site_reports(
-    client: BigQuery, config: Config, yyyymm: int, new_fn_name: str
+    client: BigQuery, yyyymm: int, new_fn_name: str
 ) -> str:
     current_table = client.get_table("scored_site_reports")
     current_query = current_table.view_query
@@ -434,7 +337,7 @@ def create_new_scored_site_reports(
     new_table = bigquery.Table(new_table_id)
     new_table.view_query = new_query
 
-    if config.write:
+    if client.write:
         logging.info(f"Creating view {new_table_id}")
         client.client.create_table(new_table, exists_ok=True)
     else:
@@ -442,18 +345,14 @@ def create_new_scored_site_reports(
     return new_table_id
 
 
-def update_site_ranks(
-    project: Project, client: BigQuery, config: Config, yyyymm: int
-) -> None:
+def update_site_ranks(project: Project, client: BigQuery, yyyymm: int) -> None:
     from . import metric_rescore
 
-    if not check_yyyymm(client, config, yyyymm):
+    if not check_yyyymm(project, client, yyyymm):
         raise ValueError(f"No site rank data found for {yyyymm}")
 
-    new_fn_name = create_new_routine(client, config, yyyymm)
-    new_site_reports = create_new_scored_site_reports(
-        client, config, yyyymm, new_fn_name
-    )
+    new_fn_name = create_new_routine(client, yyyymm)
+    new_site_reports = create_new_scored_site_reports(client, yyyymm, new_fn_name)
 
     _, new_routine_id = new_fn_name.split(".", 1)
     logging.info(new_routine_id)
@@ -490,10 +389,8 @@ class SiteRanksUpdateList(EtlJob):
         )
 
     def main(self, context: Context) -> None:
-        config = Config.from_args(context.args)
         update_site_ranks(
             context.project,
             context.bq_client,
-            config,
             context.args.site_ranks_update_yyyymm,
         )
