6e8875dcb6fa5e0ea7b1fe470fe6f40b9b15d2d0
James Graham <james@hoppipolla.co.uk>
2025-12-18 12:51:49 +0000

    Move site-rank-update job to use new rescoring logic
    This means that the `webcompat-site-rank-update` command will
    automatically generate the right schema templates rather than actually
    trying ot update prod.

--- jobs/webcompat-kb/webcompat_kb/commands/site_rank_update.py
+++ jobs/webcompat-kb/webcompat_kb/commands/site_rank_update.py
@@ -1,14 +1,15 @@
 import argparse
 import logging
+from typing import Optional
 
 from google.cloud import bigquery
 
 from . import metric_rescore
 from .. import projectdata
 from ..base import Command
-from ..bqhelpers import BigQuery, DatasetId, SchemaId, get_client
+from ..bqhelpers import BigQuery, DatasetId, SchemaId, SchemaType, get_client
 from ..config import Config
-from ..projectdata import Project
+from ..projectdata import Project, RoutineTemplate, SchemaMetadata
 
 
 def check_yyyymm(project: Project, client: BigQuery, yyyymm: int) -> bool:
@@ -21,72 +22,48 @@ SELECT EXISTS (
     return result.has_yyyymm
 
 
-def create_new_routine(client: BigQuery, yyyymm: int) -> str:
-    new_name = f"{client.project_id}.{client.default_dataset_id}.WEBCOMPAT_METRIC_YYYYMM_{yyyymm}"
-    query = f"CREATE OR REPLACE FUNCTION `{new_name}`() RETURNS INT64 AS ({yyyymm});"
-    if client.write:
-        logging.info(f"Creating function {new_name}")
-        client.query(query)
+def update_crux_routine(
+    project: Project, yyyymm: int, schema_id: SchemaId, write: bool
+) -> SchemaId:
+    template_data = f"""CREATE OR REPLACE FUNCTION `{{{{ ref(name) }}}}`() RETURNS INT64 AS (
+{yyyymm}
+);"""
+
+    schema_path = project.data.get_schema_path(SchemaType.routine, schema_id)
+    if write and not schema_path.exists():
+        raise ValueError(f"Expected {schema_path} to exist")
+    metadata = SchemaMetadata(name=schema_id.name)
+    template = RoutineTemplate(schema_path, metadata, template_data)
+    template_path = schema_path / template.filename
+    if write and not schema_path.exists():
+        raise ValueError(f"Expected {template_path} to exist")
+    if write:
+        with open(template_path, "w") as f:
+            f.write(template.template)
     else:
-        logging.info(f"Would create function {new_name}")
-    return new_name
-
-
-def create_new_scored_site_reports(
-    client: BigQuery, yyyymm: int, new_fn_name: str
-) -> str:
-    current_table = client.get_table("scored_site_reports")
-    current_query = current_table.view_query
-
-    fn_name = f"{client.project_id}.{client.default_dataset_id}.WEBCOMPAT_METRIC_YYYYMM"
-
-    if fn_name not in current_query:
-        raise ValueError(f"Failed to find {fn_name} in {current_table}")
-
-    new_query = current_query.replace(fn_name, new_fn_name)
-    new_table_id = f"{current_table.reference}_{yyyymm}"
-    new_table = bigquery.Table(new_table_id)
-    new_table.view_query = new_query
-
-    if client.write:
-        logging.info(f"Creating view {new_table_id}")
-        client.client.create_table(new_table, exists_ok=True)
-    else:
-        logging.info(f"Would create view {new_table_id} with query:\n{new_query}")
-    return new_table_id
-
-
-def update_site_ranks(project: Project, client: BigQuery, yyyymm: int) -> None:
-    if not check_yyyymm(project, client, yyyymm):
-        raise ValueError(f"No site rank data found for {yyyymm}")
-
-    new_fn_name = create_new_routine(client, yyyymm)
-    new_site_reports = create_new_scored_site_reports(client, yyyymm, new_fn_name)
-
-    _, new_routine_id = new_fn_name.split(".", 1)
-    logging.info(new_routine_id)
-
-    metric_rescore.rescore(
-        project,
-        client,
-        new_site_reports.rsplit(".", 1)[1],
-        f"Update site rank data to {yyyymm}",
-        [f"{client.default_dataset_id}.WEBCOMPAT_METRIC_YYYYMM:{new_routine_id}"],
-    )
+        logging.info(f"Would write template {template_path}:\n{template.template}")
+    return schema_id
 
 
 class SiteRankUpdate(Command):
     def argument_parser(self) -> argparse.ArgumentParser:
         parser = super().argument_parser()
         parser.add_argument(
-            "--yyyymm",
+            "phase",
+            action="store",
+            choices=["create-schemas", "prepare-deploy"],
+            help="",
+        )
+        parser.add_argument(
+            "yyyymm",
             action="store",
             type=int,
             help="New site rank data to use in the format YYYYMM",
         )
+
         return parser
 
-    def main(self, args: argparse.Namespace) -> None:
+    def main(self, args: argparse.Namespace) -> Optional[int]:
         config = Config(write=args.write, stage=args.stage)
 
         client = get_client(args.bq_project_id)
@@ -94,21 +71,44 @@ class SiteRankUpdate(Command):
             client, args.bq_project_id, args.data_path, set(), config
         )
 
-        rescores_table = SchemaId(
-            args.bq_project_id,
-            "webcompat_knowledge_base",
-            "webcompat_topline_metric_rescores",
-        )
+        kb_dataset = DatasetId(args.bq_project_id, "webcompat_knowledge_base")
+        bq_client = BigQuery(client, kb_dataset, args.write, set())
 
-        bq_client = BigQuery(
-            client, DatasetId(args.bq_project_id, ""), args.write, {rescores_table}
-        )
+        rescore = None
+        if args.phase == "create-schemas":
+            if not check_yyyymm(project, bq_client, args.yyyymm):
+                raise ValueError(f"No site rank data found for {args.yyyymm}")
 
-        update_site_ranks(
-            project,
-            bq_client,
-            args.yyyymm,
-        )
+            rescore = metric_rescore.create_schemas(
+                project,
+                bq_client,
+                args.data_path,
+                kb_dataset,
+                f"crux_{args.yyyymm}",
+                f"Update CrUX data to {args.yyyymm}",
+                ["WEBCOMPAT_METRIC_YYYYMM"],
+                args.write,
+            )
+            if rescore is not None:
+                staging_schema_id = rescore.staging_routine_ids()[
+                    SchemaId(
+                        kb_dataset.project,
+                        kb_dataset.dataset,
+                        "WEBCOMPAT_METRIC_YYYYMM",
+                    )
+                ]
+                update_crux_routine(project, args.yyyymm, staging_schema_id, args.write)
+        elif args.phase == "prepare-deploy":
+            rescore = metric_rescore.prepare_deploy(
+                project,
+                bq_client,
+                args.data_path,
+                kb_dataset,
+                f"crux_{args.yyyymm}",
+                args.write,
+            )
+
+        return 0 if rescore is not None else 1
 
 
 main = SiteRankUpdate()
