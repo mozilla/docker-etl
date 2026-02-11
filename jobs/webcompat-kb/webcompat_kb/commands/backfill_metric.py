72f5afd7eb3284c5dfc1915b898f180d240c0266
James Graham <james@hoppipolla.co.uk>
2026-02-11 09:49:46 +0000

    Add support for backfilling rank data in webcompat-backfill-metrics
    When we add a new rank we need to update the data in host_min_ranks so
    that the new field is populated.
    
    To do this we add a --rank option to `webcompat-backfill-metric` and
    require passing in --metric to also backfill the metric data.

--- jobs/webcompat-kb/webcompat_kb/commands/backfill_metric.py
+++ jobs/webcompat-kb/webcompat_kb/commands/backfill_metric.py
@@ -1,19 +1,30 @@
 import argparse
 import logging
 import os
-from typing import Optional
+from typing import Sequence, Optional
 
 from .. import projectdata
 from ..base import Command
 from ..bqhelpers import BigQuery, DatasetId, get_client
 from ..config import Config
 from ..projectdata import Project
+from ..siterank import host_min_ranks_query
+
+
+def backfill_host_min_ranks(
+    project: Project, client: BigQuery, ranks: Sequence[str]
+) -> None:
+    host_min_ranks_table = project["crux_imported"]["host_min_ranks"].table()
+
+    query = host_min_ranks_query(project, filter_yyyymm=False, ranks=ranks)
+    condition = "source.yyyymm = target.yyyymm AND source.host = target.host"
+
+    client.update_query(host_min_ranks_table, ranks, query, condition)
 
 
 def backfill_metric_daily(
     project: Project,
     client: BigQuery,
-    write: bool,
     metric_name: str,
 ) -> None:
     metric_dfns, metric_types = project.data.metric_dfns, project.data.metric_types
@@ -38,13 +49,21 @@ def backfill_metric_daily(
             f"{metric_type.agg_function('bugs', metric)} AS {field_name}"
         )
         conditions.append(f"metric_daily.{field_name} IS NULL")
+
+    scored_site_reports = project["webcompat_knowledge_base"][
+        "scored_site_reports"
+    ].view()
+    metric_daily = project["webcompat_knowledge_base"][
+        "webcompat_topline_metric_daily"
+    ].table()
+
     select_query = f"""
 SELECT
   date,
   {",\n  ".join(select_fields)}
 FROM
-  `{project["webcompat_knowledge_base"]["scored_site_reports"]}` AS bugs
-  JOIN `{project["webcompat_knowledge_base"]["webcompat_topline_metric_daily"]}` as metric_daily
+  `{scored_site_reports}` AS bugs
+  JOIN `{metric_daily}` as metric_daily
 ON
   DATE(bugs.creation_time) <= metric_daily.date
   AND IF (bugs.resolved_time IS NOT NULL, DATE(bugs.resolved_time) >= date, TRUE)
@@ -54,36 +73,47 @@ GROUP BY
   date
 ORDER BY date"""
 
-    update_query = f"""
-UPDATE `{project["webcompat_knowledge_base"]["webcompat_topline_metric_daily"]}` AS metric_daily
-SET
-  {",\n  ".join(f"metric_daily.{field_name}=new_data.{field_name}" for field_name in field_names)}
-FROM ({select_query}) AS new_data
-WHERE new_data.date = metric_daily.date
-"""
-
-    if write:
-        result = client.query(update_query)
-    else:
-        logging.info(f"Would run query:\n{update_query}")
-        result = client.query(select_query)
-        logging.info(f"Would set {list(result)}")
+    client.update_query(
+        metric_daily, field_names, select_query, "source.date = target.date"
+    )
 
 
 class BackfillMetric(Command):
     def argument_parser(self) -> argparse.ArgumentParser:
         parser = super().argument_parser()
-        parser.add_argument("metric", action="store", help="Metric name to update")
+        parser.add_argument(
+            "--rank",
+            action="append",
+            dest="ranks",
+            help="Rank name (from host_min_ranks) to backfill",
+        )
+        parser.add_argument(
+            "--metric", action="append", dest="metrics", help="Metric name to backfill"
+        )
         return parser
 
     def main(self, args: argparse.Namespace) -> Optional[int]:
         client = get_client(args.bq_project_id)
         config = Config(write=args.write, stage=args.stage)
         project = projectdata.load(
-            client, args.bq_project_id, os.path.normpath(args.data_path), set(), config
+            client,
+            args.bq_project_id,
+            os.path.normpath(args.data_path),
+            {"metric"},
+            config,
         )
-        if args.metric not in {item.name for item in project.data.metric_dfns}:
-            raise ValueError(f"Unknown metric {args.metric}")
+
+        for name, valid_values, arg_values in [
+            ("metrics", {item.name for item in project.data.metric_dfns}, args.metrics),
+            ("ranks", {item.name for item in project.data.rank_dfns}, args.ranks),
+        ]:
+            if arg_values:
+                unknown_values = set(arg_values) - valid_values
+                if unknown_values:
+                    logging.error(
+                        f"Unknown {name}: {' '.join(unknown_values)}, options are {' '.join(valid_values)}"
+                    )
+                    return 1
 
         bq_client = BigQuery(
             client,
@@ -92,12 +122,16 @@ class BackfillMetric(Command):
             None,
         )
 
-        backfill_metric_daily(
-            project,
-            bq_client,
-            config.write,
-            args.metric,
-        )
+        if args.ranks:
+            backfill_host_min_ranks(project, bq_client, args.ranks)
+
+        if args.metrics:
+            for metric in args.metrics:
+                backfill_metric_daily(
+                    project,
+                    bq_client,
+                    metric,
+                )
         return None
 
 
