3f3859a029d0d8cfb6cc4ea57f7dd2a25dba990e
jgraham <james@hoppipolla.co.uk>
2026-02-13 19:13:30 +0000

    Add ty to webcompat_kb testing (#479)
    * Fix ty errors
    
    * Start to run ty as part of tests
    
    * Add ty to CI

--- jobs/webcompat-kb/test.sh
+++ jobs/webcompat-kb/test.sh
@@ -4,5 +4,6 @@ set -ex
 
 uv sync --extra=test
 uv run mypy webcompat_kb
+uv run ty check webcompat_kb
 uv run pytest --ruff --ruff-format .
 uv run webcompat-check-templates --bq-project="moz-fx-dev-dschubert-wckb"
