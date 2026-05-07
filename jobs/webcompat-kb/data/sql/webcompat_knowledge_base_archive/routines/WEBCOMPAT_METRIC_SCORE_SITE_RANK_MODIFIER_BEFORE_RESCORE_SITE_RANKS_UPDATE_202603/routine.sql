7f99916ccf8798020fbbc079f10237539d236851
Ksenia <kberezina@mozilla.com>
2026-05-07 15:59:53 -0400

    Fix a ref to dim_bug_score (#520)

--- jobs/webcompat-kb/data/sql/webcompat_knowledge_base_archive/routines/WEBCOMPAT_METRIC_SCORE_SITE_RANK_MODIFIER_BEFORE_RESCORE_SITE_RANKS_UPDATE_202603/routine.sql
+++ jobs/webcompat-kb/data/sql/webcompat_knowledge_base_archive/routines/WEBCOMPAT_METRIC_SCORE_SITE_RANK_MODIFIER_BEFORE_RESCORE_SITE_RANKS_UPDATE_202603/routine.sql
@@ -3,9 +3,9 @@ CREATE OR REPLACE FUNCTION `{{ ref(name) }}`(url STRING, crux_yyyymm INT64) RETU
      IFNULL(IF(MIN(host_ranks.global_rank) <= MIN(IFNULL(host_ranks.local_rank, host_ranks.global_rank)), MAX(1.5 * global_scores.score), MAX(local_scores.score)), 1)
    FROM
      `{{ ref ('crux_imported.host_min_ranks') }}` AS host_ranks
-   LEFT JOIN `{{ ref('dim_bug_score') }}` AS local_scores ON local_scores.lookup_type = 'site_rank' AND host_ranks.local_rank <= cast(local_scores.lookup_value as int64)
-   LEFT JOIN `{{ ref('dim_bug_score') }}` AS global_scores ON global_scores.lookup_type = 'site_rank' AND host_ranks.global_rank <= cast(global_scores.lookup_value as int64)
+   LEFT JOIN `{{ ref('webcompat_knowledge_base.dim_bug_score') }}` AS local_scores ON local_scores.lookup_type = 'site_rank' AND host_ranks.local_rank <= cast(local_scores.lookup_value as int64)
+   LEFT JOIN `{{ ref('webcompat_knowledge_base.dim_bug_score') }}` AS global_scores ON global_scores.lookup_type = 'site_rank' AND host_ranks.global_rank <= cast(global_scores.lookup_value as int64)
    WHERE
-     host_ranks.yyyymm = crux_yyyymm AND `{{ ref('WEBCOMPAT_HOST') }}`(host_ranks.host) = `{{ ref('WEBCOMPAT_HOST') }}`(url)
+     host_ranks.yyyymm = crux_yyyymm AND `{{ ref('webcompat_knowledge_base.WEBCOMPAT_HOST') }}`(host_ranks.host) = `{{ ref('webcompat_knowledge_base.WEBCOMPAT_HOST') }}`(url)
   )
 );
