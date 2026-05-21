2619b0f35853417bb1866c3cc29a6314b50ec856
Andrew Halberstadt <ahal@mozilla.com>
2026-05-21 12:07:18 -0400

    Bug 2041074 - Skip records that exceed BigQuery's max record limit (#528)

--- jobs/fxci-taskcluster-export/test/test_bigquery_loader.py
+++ jobs/fxci-taskcluster-export/test/test_bigquery_loader.py
@@ -3,8 +3,12 @@ from types import SimpleNamespace
 from fxci_etl.loaders.bigquery import BigQueryLoader
 
 
-def _make_loader(chunk_size, max_bytes):
-    return SimpleNamespace(chunk_size=chunk_size, max_insert_bytes=max_bytes)
+def _make_loader(chunk_size, max_bytes, max_row_bytes=None):
+    return SimpleNamespace(
+        chunk_size=chunk_size,
+        max_batch_bytes=max_bytes,
+        max_row_bytes=max_row_bytes if max_row_bytes is not None else max_bytes,
+    )
 
 
 def test_chunk_batches_respect_record_count():
@@ -36,16 +40,15 @@ def test_chunk_batches_respect_serialized_size():
     ]
 
 
-def test_chunk_batches_allow_oversized_single_row():
+def test_chunk_batches_skip_oversized_row():
     records = [
         {"value": "x" * 100},
         {"value": "y"},
     ]
-    loader = _make_loader(chunk_size=100, max_bytes=50)
+    loader = _make_loader(chunk_size=100, max_bytes=1000, max_row_bytes=50)
 
     batches = list(BigQueryLoader._chunk_batches(loader, records))
 
     assert batches == [
-        [{"value": "x" * 100}],
         [{"value": "y"}],
     ]
