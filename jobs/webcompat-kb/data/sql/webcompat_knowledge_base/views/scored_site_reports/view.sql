177bc9737d776c099b9d6f64f538b09e705d3b1e
James Graham <james@hoppipolla.co.uk>
2026-08-06 09:49:07 +0100

    Allow override of site rank in webcompat score using user-story data
    This is useful for cases where we think the domain rank isn't
    representative of the real impact of the problem e.g. for code that is
    deployed on many different domains or subdomains.

--- jobs/webcompat-kb/data/sql/webcompat_knowledge_base/views/scored_site_reports/view.sql
+++ jobs/webcompat-kb/data/sql/webcompat_knowledge_base/views/scored_site_reports/view.sql
@@ -62,10 +62,8 @@ These could be inlined, but it's slightly easier to read if they're computed in 
 computed_scores AS (
   SELECT
     number,
-    `{{ ref('WEBCOMPAT_METRIC_SCORE_NO_SITE_RANK') }}`(keywords,
-      user_story) AS triage_score_no_rank,
-    `{{ ref('WEBCOMPAT_METRIC_SCORE_SITE_RANK_MODIFIER') }}`(url,
-      `{{ ref('WEBCOMPAT_METRIC_YYYYMM') }}`()) AS site_rank_score
+    `{{ ref('WEBCOMPAT_METRIC_SCORE_NO_SITE_RANK') }}`(keywords, user_story) AS triage_score_no_rank,
+    `{{ ref('WEBCOMPAT_METRIC_SCORE_SITE_RANK_MODIFIER') }}`(url, `{{ ref('WEBCOMPAT_METRIC_YYYYMM') }}`(), user_story) AS site_rank_score
   FROM
     `{{ ref('site_reports') }}` AS site_reports
 ),
