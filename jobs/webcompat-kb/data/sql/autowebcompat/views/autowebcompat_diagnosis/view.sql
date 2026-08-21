af18aadf607c709c53b2155fb870b49b44026c79
Benoit Chauvet <compay2k@gmail.com>
2026-08-21 14:45:44 +0200

    remove duplicates exclusion

--- jobs/webcompat-kb/data/sql/autowebcompat/views/autowebcompat_diagnosis/view.sql
+++ jobs/webcompat-kb/data/sql/autowebcompat/views/autowebcompat_diagnosis/view.sql
@@ -21,10 +21,6 @@ hackbot_diagnosis AS (
   JOIN `{{ ref('hackbot_completed') }}` completed USING (run_id)
   WHERE scheduled.task_name = 'diagnosis'
   AND STARTS_WITH(scheduled.source_key, "bugzilla:")
-  QUALIFY ROW_NUMBER() OVER (
-    PARTITION BY PARSE_NUMERIC(scheduled.run_key)
-    ORDER BY completed.completed_at DESC
-  ) = 1
 )
 SELECT
 reports.number AS number,
