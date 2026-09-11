20f9caefd2844b16f78a814236213f91b2550dbb
jgraham <james@hoppipolla.co.uk>
2026-09-11 14:48:03 +0100

    Include explictly linked bugs in interop_bugs view (#593)

--- jobs/webcompat-kb/data/sql/interop/views/interop_bugs/view.sql
+++ jobs/webcompat-kb/data/sql/interop/views/interop_bugs/view.sql
@@ -12,12 +12,27 @@ feature_bugs AS (
   JOIN `{{ ref('webcompat_knowledge_base.bugs_platform_data') }}` as bugs ON bugs.feature IN UNNEST(interop_proposals.features)
 ),
 
+see_also_bugs_all AS (
+  SELECT number, CAST(ARRAY_LAST(SPLIT(`{{ ref('webcompat_knowledge_base.URL_PARSE') }}`(see_also).path, "/")) as INTEGER) as issue
+  FROM `{{ ref('webcompat_knowledge_base.bugzilla_bugs') }}`
+  JOIN UNNEST(see_also) AS see_also
+  WHERE starts_with(see_also, "https://github.com/web-platform-tests/interop/issues/")
+),
+
+see_also_bugs AS (
+  SELECT number, issue
+  FROM see_also_bugs_all
+  JOIN `{{ ref('interop_proposals') }}` AS interop_proposals USING(issue)
+),
+
 all_bugs AS (
   SELECT issue, ARRAY_AGG(number) as bugs FROM (
     SELECT DISTINCT issue, number FROM (
       SELECT issue, number FROM linked_bugs
       UNION ALL
       SELECT issue, number FROM feature_bugs
+      UNION ALL
+      SELECT issue, number FROM see_also_bugs
     )
   )
   GROUP BY issue
